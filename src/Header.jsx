76688b688bccc69caec6d9d8a2cea0c1d1f1150a
imran idris <imranidris10999@gmail.com>
2026-05-07 10:21:38 +0300

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 function Header({ count }) {
   return (
     <div className="container">
-      <h1>Counter Mini App</h1>
+      <h1> Mini Counter App</h1>
       <h3>{count}</h3>
     </div>
   );
